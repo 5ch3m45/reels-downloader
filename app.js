0e0e2d2f863e1f7966219a83f56c93796561f8bd
5ch3m45 <maulanshory@gmail.com>
2024-10-17 14:58:02 +0700

    remove cheerio

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const cheerio = require('cheerio')
 const querystring = require('querystring')
 
 const getPostIdFromUrl = (postUrl) => {
